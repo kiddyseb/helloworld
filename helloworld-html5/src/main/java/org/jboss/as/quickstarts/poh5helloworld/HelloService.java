c8e3d8dcaf146898964f8fb5f8420c0db54d700d
zentelia <martin.petitclerc@sympatico.ca>
2012-10-29 22:27:46 -0400

    Bonjour to Salut

--- helloworld-html5/src/main/java/org/jboss/as/quickstarts/poh5helloworld/HelloService.java
+++ helloworld-html5/src/main/java/org/jboss/as/quickstarts/poh5helloworld/HelloService.java
@@ -9,7 +9,7 @@ package org.jboss.as.quickstarts.poh5helloworld;
 public class HelloService {
 
    String createHelloMessage(String name) {
-      return "Bonjour " + name + "!";
+      return "Salut " + name + "!";
    }
 
 }
